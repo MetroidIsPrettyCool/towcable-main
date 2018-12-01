b149695458be2173d1f5ee568d96e57e2e51e95c
HaHaIHaveAShare <43505647+HaHaIHaveAShare@users.noreply.github.com>
2018-12-01 14:24:48 -0800

    Changed AEND version to AE6

--- TSNI0_3.c
+++ TSNI0_3.c
@@ -8,32 +8,37 @@ const int pastAmount = 100; // The maximum amount of time you can go back with P
   Notes:
 ________________________________________________________________________________________________________________________________________________________________________________________________________  
   Token values:
-     Numbers          : -1 * (value + 1). I.E. 0 = -1, 1 = -2, 12 = -13, 117 = -118, etc.
-     Symbols          : & = 100 . = 101 @ = 200 + = 201 - = 202
-     Numerical Ops    : RAND = 300 ADDN = 301 SUBN = 302 MULN = 303 DIV = 304 MODN = 305 PAST = 306
-     IFs              : EQ = 400 NQ = 401 GT = 402 LT = 403 GE = 404 LE = 405
-     Setup Commands   : DIMN = 500 SIZE = 501 SETP = 502 RULE = 503 CMPX = 504 BEGN = 505
-     Runtime Commands : OUTP = 600 CRPT = 601 INPT = 602 CALL = 603 CALX = 604 LOOP = 605 LOTO = 606 COND = 607 ENDC = 608 STRO = 609 EXIT = 610
-     Special          : 10 is the beginning of the array, and is ignored. 0 is a blank value.
-     
+     Nothing       : 0
+     Numbers       : -1 * (value + 1). I.E. 0 = -1, 1 = -2, 12 = -13, 117 = -118, etc.
+     Symbols       : @ = 200 + = 201 - = 202 & = 100
+     Numerical Ops : RAND = 300 ADDN = 301 SUBN = 302 MULN = 303 DIV = 304 MODN = 305 PAST = 306 LOCN = 307
+     IFs           : EQ = 400 NQ = 401 GT = 402 GE = 403 LT = 404 LE = 405
   
-   Miscellanea:
+     Special       : 10 is the beginning of the array, and is ignored. 0 is a blank value.
+  
+  Miscellanea:
      The old base 64 single-digit system has been overturned in favor of a base ten system. All whole numbers are valid (floating point and negative numbers may come in a revision of the languge)
      The reason no 1xx tokens are used is they originally referred to base 64 numbers, and I'm lazy and haven't changed the token scheme. (And probably won't) (& was added after this was typed)
      PAST support is coming, probably in AE4
      PAST in 2D will require a bit of special code, as it will use 3 values (time, x coordinate, y coordinate)
      TSNI will use the tokens values desribed above, because I'm lazy :P (I'm not sure why this was listed under "Things to come in AE4")
+  AE5 new features:
+     
+     Infinite dimensions!
+     Support for EQ, NQ, GT, LT, GE and LE
+     Removal of comments (sorry future me)
   
   License:
-     DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-     Version 2, December 2004
-     Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
-     Everyone is permitted to copy and distribute verbatim or modified
-     copies of this license document, and changing it is allowed as long
-     as the name is changed.
-     DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-     TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
-     0. You just DO WHAT THE FUCK YOU WANT TO.
+      
+      DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+      Version 2, December 2004
+      Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+      Everyone is permitted to copy and distribute verbatim or modified
+      copies of this license document, and changing it is allowed as long
+      as the name is changed.
+      DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+      TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+      0. You just DO WHAT THE FUCK YOU WANT TO.        
 ________________________________________________________________________________________________________________________________________________________________________________________________________                   
 */
 
@@ -53,68 +58,29 @@ int AEND (int *tokens, int tokenAmount, int *cell, int *cellSizes, int dimension
     printf("This is a dead meme\n");
     return 609;
   }
-  printf("b\n");
   for (;;)  {
-    for (i = tokenAmount; *(tokens + i) <= 0; i--);
+    for (i = tokenAmount; *(tokens + i) <= 300 && *(tokens + i) != 10; i--) {
+      printf("t\n");
+      fflush(NULL);
+    }
     currMultiOp = *(tokens + i);
+    printf("qwqw%i  %i\n", currMultiOp, *(tokens + i));
+    fflush(NULL);
     currLoc = i;
     if (currMultiOp != 10)  {
-      for (i2 = 0; i2 != ((currMultiOp == 306) ? dimensions + 1 : ((currMultiOp > 300) ? 2 : dimensions)); i2++)  {
-	for (i = currLoc; *(tokens + i) >= 0; i++);
-	*(valS + i2) = (*(tokens + i) * -1) - 1;
+      for (i2 = 0; i2 != ((currMultiOp == 306) ? dimensions + 1 : ((currMultiOp == 307) ? (2 * dimensions) : 2)); i2++)  {
+	for (i = currLoc; *(tokens + i) >= 300 || *(tokens + i) == 0; i++);
+	printf("%i 12rewqasd;'\n", *(tokens + i));
+        if (*(tokens + i) < 0)  {
+	  *(valS + i2) = (*(tokens + i) * -1) - 1;
+	}
+	else {
+	  *(valS + i2) = *(tokens + i);
+	}
 	*(tokens + i) = 0;
       }
-      printf("c\n");
-      if (currMultiOp == 200)  {
-	for (i2 = 0; i2 != dimensions; i2++)  {
-	  while (*(valS + i2) >= *(cellSizes + i2))  {
-	    *(valS + i2) -= *(cellSizes + i2);
-	  }
-	}
-	i3 = 0;
-	for (i2 = 0; i2 != dimensions; i2++)  {
-	  i4 = 1;
-	  for (i5 = 0; i5 <= i2 - 1; i5++)  {
-	    i4 *= *(cellSizes + i5);
-	  }
-	  i3 += *(valS + i2) * i4;
-	}
-	*(tokens + currLoc) = (*(cell + i3) + 1) * -1;
-	
-      }
-      if (currMultiOp == 201)  {
-	
-        for (i2 = 0; i2 != dimensions; i2++)  {
-	  while (*(valS + i2) + *(cellPointerCoors + i2) >= *(cellSizes + i2))  {
-	    *(valS + i2) -= *(cellSizes + i2);
-	  }
-	}
-	i3 = 0;
-	for (i2 = 0; i2 != dimensions; i2++)  {
-	  i4 = 1;
-	  for (i5 = 0; i5 <= i2 - 1; i5++)  {
-	    i4 *= *(cellSizes + i5);
-	  }
-	  i3 += (*(valS + i2) + *(cellPointerCoors + i2)) * i4;
-	}
-	*(tokens + currLoc) = (*(cell + i3) + 1) * -1;
-      }
-      if (currMultiOp == 202)  {
-        for (i2 = 0; i2 != dimensions; i2++)  {
-	  while (*(cellPointerCoors + i2) - *(valS + i2) <= 0)  {
-	    *(valS + i2) += *(cellSizes + i2);
-	  }
-	}
-	i3 = 0;
-	for (i2 = 0; i2 != dimensions; i2++)  {
-	  i4 = 1;
-	  for (i5 = 0; i5 <= i2 - 1; i5++)  {
-	    i4 *= *(cellSizes + i5);
-	  }
-	  i3 += (*(cellPointerCoors + i2) - *(valS + i2)) * i4;
-	}
-	*(tokens + currLoc) = (*(cell + i3) + 1) * -1;
-      }
+      printf("qwqw%i\n", currMultiOp);
+      fflush(NULL);
       if (currMultiOp == 300)  {
  	*(tokens + currLoc) = (((rand() % (*(valS + 1))) + *valS) + 1) * -1;
       }
@@ -147,6 +113,58 @@ int AEND (int *tokens, int tokenAmount, int *cell, int *cellSizes, int dimension
 	}
 	i3 += *(valS + dimensions) * i4 * pastAmount;
 	*(tokens + currLoc) = (*(pastCell + i3) + 1) * -1;
+      }
+      if (currMultiOp == 307)  { // LOCN
+	i3 = 0;
+	printf("g\n");
+	fflush(NULL);
+	printf("%i %i gg234234\n", *valS, *(valS + 1));
+	for (i2 = 0; i2 != dimensions; i2++)  {
+	  if (*(valS + (2 * i2)) == 200) {
+	    printf("wqwhiojk\n");
+	    while (*(valS + (2 * i2) + 1) > *(cellSizes + i2))  {
+	      printf("r\n");
+	      *(valS + (2 * i2) + 1) -= *(cellSizes + i2);
+	    }
+	    i4 = 1;
+	    for (i5 = 0; i5 < i2; i5++)  {
+	      i4 *= *(cellSizes + i5);
+	    }
+	    printf("%i rtqw\n", i4);
+	    i3 += *(valS + (2 * i2 + 1)) * i4;
+	    printf("%i 8901243\n", i3);
+	  }
+	  if (*(valS + (2 * i2)) == 201) {
+	    printf("wqwhiojk\n");
+	    while (*(cellPointerCoors + i2) + *(valS + (2 * i2) + 1) > *(cellSizes + i2))  {
+	      printf("r\n");
+	      *(valS + (2 * i2) + 1) -= *(cellSizes + i2);
+	    }
+	    i4 = 1;
+	    for (i5 = 0; i5 < i2; i5++)  {
+	      i4 *= *(cellSizes + i5);
+	    }
+	    printf("%i rtqw\n", i4);
+	    i3 += (*(cellPointerCoors + i2) + *(valS + (2 * i2 + 1))) * i4;
+	    printf("%i 8901243\n", i3);
+	  }
+	  if (*(valS + (2 * i2)) == 202) {
+	    printf("wqwhiojk\n");
+	    while (*(cellPointerCoors + i2) - *(valS + (2 * i2) + 1) > *(cellSizes + i2))  {
+	      printf("r\n");
+	      *(valS + (2 * i2) + 1) -= *(cellSizes + i2);
+	    }
+	    i4 = 1;
+	    for (i5 = 0; i5 < i2; i5++)  {
+	      i4 *= *(cellSizes + i5);
+	    }
+	    printf("%i rtqw\n", i4);
+	    i3 += (*(cellPointerCoors + i2) - *(valS + (2 * i2 + 1))) * i4;
+	    printf("%i 8901243\n", i3);
+	  }
+	}
+	printf("yart %i\n", *(cell + i3));
+	*(tokens + currLoc) = (*(cell + i3) + 1) * -1;
       }
       if (currMultiOp == 400)  {
 	if (*valS == *(valS + 1))  {
@@ -198,12 +216,10 @@ int AEND (int *tokens, int tokenAmount, int *cell, int *cellSizes, int dimension
       }
     }
     else {
-      printf("d\n");
       return (*(tokens + 1) * -1) -1;
     }
   }
 }
-
 
 int main (void)  {
   srand(time(0)); // Seed the Random Number Generator with the current time
