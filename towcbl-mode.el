76096a7162f3e7db58b093f0ea4da36a8d181ec5
Joseph Burke <43505647+HaHaIHaveAShare@users.noreply.github.com>
2018-12-26 16:29:43 -0800

    Improved and bugfixed comments and strings

--- towcbl-mode.el
+++ towcbl-mode.el
@@ -2,17 +2,23 @@
 
 (defvar towcbl-font-lock-keywords)
 (setq towcbl-font-lock-keywords
-      '(("\".*\"" . font-lock-string-face)
+      '(
 	("[0-9]+i" . font-lock-constant-face)
-	("!.*!" . font-lock-comment-face)
 	("@\\|\+\\|\-\\|&" . font-lock-warning-face)
 	("\\(\\(ADD\\|SUB\\|MUL\\|DIV\\|MOD\\|LOC\\)N\\)\\|RAND\\|PAST" . font-lock-function-name-face)
 	("DIMN\\|SIZE\\|SETP\\|RULE\\|CMPX\\|BEGN\\|OUPT\\|CRPT\\|INPT\\|CALL\\|CALX\\|LOOP\\|LOTO\\|COND\\|ENDC\\|STRO\\|EXIT" . font-lock-builtin-face)
 	("\\(E\\|N\\)Q\\|\\(G\\|L\\)\\(T\\|E\\)" . font-lock-keyword-face)))
 
+(defvar towcbl-mode-syntax-table nil "Syntax table for `towcbl-mode'.")
+
+(setq towcbl-mode-syntax-table
+      (let ( (synTable (make-syntax-table)))
+        ;; python style comment: “# …”
+        (modify-syntax-entry ?! "!" synTable)
+        synTable))
+
 ;;;###autoload
-(define-derived-mode towcbl-mode fundamental-mode
-  "towcbl"
+(define-derived-mode towcbl-mode prog-mode "towcbl"
   "Major mode for towcbl source code."
   (setq font-lock-defaults '(towcbl-font-lock-keywords))
   )
